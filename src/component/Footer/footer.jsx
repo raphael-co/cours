0c09a9782d1bf99548e4366bb4ea192cd26658df
raphael-co <76869389+raphael-co@users.noreply.github.com>
2021-10-27 22:47:19 +0200

    new design

--- src/component/Footer/footer.jsx
+++ src/component/Footer/footer.jsx
@@ -1,35 +1,44 @@
 import style from './footer.module.css'
+import DATA from '../assets/json/info.js'
 // import PropTypes from 'prop-types'
 
-export default function Footer(backgroundColor,...props) {
+export default function Footer(backgroundColor, ...props) {
+
+    console.log(DATA)
     return (
 
 
 
         // <footer className={style.hello}>
-        <footer className={style.hello}
-        style={backgroundColor && {backgroundColor}}
+        <footer
+            style={backgroundColor && { backgroundColor }}
         >
             <div className={style.box}>
                 <div className={style.one}>
-                    <h5
+                    <h5 id={style.titre}
                     //  style={args.footer}
-                     >titre 1</h5>
-                    <p>lorem ipsum dolor</p>
-                    <p>lorem ipsum dolor</p>
-                    <p>lorem ipsum dolor</p>
-                    </div>
+                    >{DATA.NousContacter.NousContacter}</h5>
+                    <hr />
+                    <p id={style.info}>{DATA.NousContacter.adresse}</p>
+                    <p id={style.info}>{DATA.NousContacter.mail}</p>
+                    <p id={style.info}>{DATA.NousContacter.telephone}</p>
+                </div>
+                {/* #399992 */}
                 <div className={style.two}>
-                    <h5>Qui sommes nous</h5>
-                    <p>lorem ipsum dolor</p>
-                    <p>lorem ipsum dolor</p>
-                    <p>lorem ipsum dolor</p>
+                    <h5 id={style.titre}>{DATA.QuiSommesNous.QuiSommesNous}</h5>
+                    <hr />
+                    <p id={style.info}>{DATA.QuiSommesNous.creation}</p>
+                    <p id={style.info}>{DATA.QuiSommesNous.fondateur}</p>
+                    <p id={style.info}>{DATA.QuiSommesNous.ChiffreA}</p>
                 </div>
                 <div className={style.three}>
-                    <h5>nous contacter</h5>
-                    <p>lorem ipsum dolor</p>
-                    <p>lorem ipsum dolor</p>
-                    <p>lorem ipsum dolor</p>
+                    <h5 id={style.titre}>{DATA.NosServices.NosServices}</h5>
+                    <hr />
+                    <p id={style.info}>{DATA.NosServices.TousNosConseils}</p>
+                    <p id={style.info}>{DATA.NosServices.Devis}</p>
+                    <p id={style.info}>{DATA.NosServices.EspaceClient}</p>
+
+
                 </div>
             </div>
         </footer>
---
+++ src/component/assets/json/info.js
@@ -0,0 +1,24 @@
+var DATA = {
+    "NousContacter": {
+        "NousContacter": "Nous Contacter",
+        "mail": "email@email.com",
+        "telephone": "0102030405",
+        "adresse": "1 rue du louvre, Paris"
+    },
+    "QuiSommesNous": {
+        "QuiSommesNous": "Qui sommes nous",
+        "creation": "entreprise créée en 2001",
+        "fondateur":"Comandon Raphael",
+        "ChiffreA":"1 000 adhérents"
+
+    },
+    "NosServices": {
+        "NosServices":"Nos Services",
+        "EspaceClient": "Espace client",
+        "Devis": "Devis en ligne",
+        "TousNosConseils": "Tous nos conseils"
+    }
+}
+
+export default DATA;
+
